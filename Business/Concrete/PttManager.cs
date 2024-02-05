7fc058d5c0a9b4128cfdaf2ca63b377c256ec7bc
Coder <Coder@DESKTOP-JT59BER>
2024-02-05 22:09:38 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,31 @@
+﻿
+using Business.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class PttManager:ISupplierManager
+{
+    private IApplicantService _applicantService;
+
+    public PttManager(IApplicantService applicantService)
+    {
+         _applicantService = applicantService;
+    }
+    public void GiveMask(Person person)
+    {
+        if (_applicantService.CheckPerson(person))
+        {
+            Console.WriteLine("Maske verildi"+person.FirstName);
+        }
+        else
+        {
+            Console.WriteLine("Maske verilemedi "+person.FirstName);
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading.Tasks;
+using Business.Concrete;
+using Entities;
+
+
+
+PttManager pttManager = new(new PersonManager());
+pttManager.GiveMask(new Person() { FirstName = "Deneme", LastName = "Deneme", 
+    DateOfBirthYear = 2000, NationalIdentity = 26741503420 });
